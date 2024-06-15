5841df6b284d0a85421e68daf846fdba0f8d0c38
Benjamin Schiller <bs@s42m.de>
2024-06-15 14:43:45 +0200

    Replaced string based charset lookup with StandardCharsets.UTF_8

--- src/main/java/de/s42/log/impl/FileAndConsoleLogger.java
+++ src/main/java/de/s42/log/impl/FileAndConsoleLogger.java
@@ -1,19 +1,19 @@
 // <editor-fold desc="The MIT License" defaultstate="collapsed">
 /*
  * The MIT License
- * 
+ *
  * Copyright 2021 Studio 42 GmbH (https://www.s42m.de).
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,7 +30,7 @@ import de.s42.log.LogLevel;
 import de.s42.log.Logger;
 import static de.s42.log.impl.ConsoleLogger.timersPerThread;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -337,7 +337,7 @@ public class FileAndConsoleLogger extends ConsoleLogger
 			}
 
 			// @todo https://www.happycoders.eu/de/java/dateien-schnell-einfach-schreiben/
-			Files.writeString(file, logLine, Charset.forName("UTF-8"), StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+			Files.writeString(file, logLine, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
